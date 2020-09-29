ab82a0a3f80cf1d130341a1113b46db14c8cdfc7
AJ Markow <alexanderjmarkow@gmail.com>
2020-09-29 16:17:17 -0700

    Update variable names and references throughout business logic files.
    Co-authored-by: Ophelia Swen <ophelia.swen@gmail.com>

--- app.rb
+++ app.rb
@@ -5,20 +5,24 @@ also_reload("lib/**/*.rb")
 
 get("/riddles") do
   testriddle = Riddles.new
-  @riddlenumber = testriddle.get_random()
-  @displayedriddle = testriddle.get_riddle(@riddlenumber, "riddle")
+  riddlenumber = testriddle.get_random()
+  @displayedriddle = testriddle.get_riddle(riddlenumber, "riddle")
   erb(:showriddle)
 end
 
-get('/riddles/:key') do
-end
+# get("/riddles/:key") do
+# end
+
+# patch("/riddles") do
+# end
 
 post("/riddles") do
   testriddle = Riddles.new
   random = testriddle.get_random()
-  if testriddle.check_answer(@riddlenumber, @user_answer) == true
+  answer = @user_answer
+  if testriddle.check_answer(@random_number, answer) == true
     erb(:success)
-  elsif testriddle.check_answer(@riddlenumber, @user_answer) == false
+  elsif testriddle.check_answer(@random_number, answer) == false
     erb(:wrong)
   end
 end
--- lib/riddles.rb
+++ lib/riddles.rb
@@ -6,33 +6,33 @@ class Riddles
 
   def initialize()
     @theriddlehash = riddle_storage
-    @riddle_answer = riddle_answer
+    @theanswerhash = riddle_answer
     @user_answer = user_answer
+    @random_number = 0
   end
 
   def get_random
-    riddlenumber = random_number = rand(3)
-    return @theriddlehash = riddlenumber
+    return @random_number = rand(3)
   end
 
-  def get_riddle(random_number,riddleoranswer)
+  def get_riddle(random_number, riddleoranswer)
     riddleoranswer.to_str
     if riddleoranswer == "riddle"
-   @theriddlehash = Hash.new
-    @theriddlehash.store(0, "What goes up but never comes down?")
-    @theriddlehash.store(1, "I shave every day, but my beard stays the same. What am I?")
-    @theriddlehash.store(2, "What can you break, even if you never pick it up or touch it?")
-    @theriddlehash.store(3,"What can’t talk but will reply when spoken to?")
-     return @theriddlehash.fetch(random_number)
+      @theriddlehash = Hash.new
+      @theriddlehash.store(0, "What goes up but never comes down?")
+      @theriddlehash.store(1, "I shave every day, but my beard stays the same. What am I?")
+      @theriddlehash.store(2, "What can you break, even if you never pick it up or touch it?")
+      @theriddlehash.store(3, "What can’t talk but will reply when spoken to?")
+      answer = @theriddlehash.fetch(@random_number)
     elsif riddleoranswer == "answer"
-   @theanswerhash = Hash.new
-    @theanswerhash.store(0, "Your Age")
-    @theanswerhash.store(1, "A barber")
-    @theanswerhash.store(2, "A promise")
-    @theanswerhash.store(3, "An echo")
-    return @theanswerhash.fetch(random_number)
+      @theanswerhash = Hash.new
+      @theanswerhash.store(0, "Your Age")
+      @theanswerhash.store(1, "A barber")
+      @theanswerhash.store(2, "A promise")
+      @theanswerhash.store(3, "An echo")
+      answer = @theanswerhash.fetch(@random_number)
     end
-   
+    return answer
   end
 
   def pick_answer
@@ -40,13 +40,16 @@ class Riddles
   end
 
   def check_answer(random_number, answer)
-    answertocheck = self.get_riddle(random_number, 'answer')
+    answertocheck = self.get_riddle(random_number, "answer")
     puts answertocheck
     puts @user_answer
-    if  @user_answer == answertocheck
-      return true
+    if @user_answer == answertocheck
+      puts true
+      return answer = true
     elsif @user_answer != answertocheck
-      return false
+      puts false
+      return answer = false
     end
+    return answer
   end
 end
